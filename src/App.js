396d81f07f6535dd457b42b3dcaa0427fd7352b8
Gwen <yjgwen@gmail.com>
2022-05-25 20:53:59 +0900

    키워드 - 항해99 react 입문 주차 개인 과제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import { Route } from "react-router-dom";
+
+import Home from "./Home";
+import Review from "./Review";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/review/:weekdays" exact componet={Review}>
+        <Review />
+      </Route>
     </div>
   );
 }
+
 
 export default App;
